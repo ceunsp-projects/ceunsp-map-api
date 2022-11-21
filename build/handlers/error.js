37b9ee6e8054e1dba482a42b041881c68cd4483b
Lucas Cefali <lucas.cefali@gmail.com>
2022-11-21 11:21:13 -0300

    fix: build folder

--- build/handlers/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res) => {
-    res.status(500);
-    return res.render('error', { error: err });
-};
-exports.default = errorHandler;
